12c8045bd223dd6c0f86e1b18fd04b8a64e239fe
XanVranck <vranckaert.xan@gmail.com>
2018-05-17 18:48:39 +0200

    first menu component vue material

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,18 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <p>{{helloWorld}}</p>
+    <edulynx-menu/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import edulynxMenu from './components/EDULYNX-Menu.vue'
 import axios from "axios";
 
 
 export default {
   name: 'app',
-  data:function() {return {helloWorld: String}}, 
   components: {
-    HelloWorld
+    edulynxMenu
   }, created() {
             axios({ method: "GET", "url": "http://localhost:8080/exploration-days-backend" }).then(result => {
                 this.helloWorld = result.data;
@@ -27,14 +24,3 @@ export default {
         },
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import 'vue-material/dist/vue-material.min.css';
+import { MdList, MdIcon, MdSwitch, MdCheckbox } from 'vue-material/dist/components';
+
+Vue.use(MdList);
+Vue.use(MdIcon);
+Vue.use(MdSwitch);
+Vue.use(MdCheckbox);
 
 Vue.config.productionTip = false
+
+Vue.component('edulynxMenu', require('./components/EDULYNX-Menu.vue'));
 
 new Vue({
   render: h => h(App)
