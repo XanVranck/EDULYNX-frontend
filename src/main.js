298b119c5d2ea4335133ceb184d6bab114ad2ea2
XanVranck <vranckaert.xan@gmail.com>
2018-05-18 07:48:16 +0200

    landing page + added mdButton

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import Vue from 'vue';
 import Edulynx from './Edulynx.vue';
 import 'vue-material/dist/vue-material.min.css';
-// import 'vue-material/dist/theme/black-green-light.css'
 import router from './router/router.js'
-import { MdList, MdIcon, MdSwitch, MdCheckbox, MdToolbar, MdContent, MdCard } from 'vue-material/dist/components';
+import 'vue-material/dist/theme/black-green-dark.css'
+import { MdList, MdIcon, MdSwitch, MdCheckbox, MdToolbar, MdContent, MdCard, MdButton } from 'vue-material/dist/components';
 
 Vue.use(MdList);
 Vue.use(MdIcon);
@@ -12,6 +12,7 @@ Vue.use(MdCheckbox);
 Vue.use(MdToolbar);
 Vue.use(MdContent);
 Vue.use(MdCard);
+Vue.use(MdButton);
 
 Vue.config.productionTip = false;
 
--- src/router/router.js
+++ src/router/router.js
@@ -1,13 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Edulynx from '@/Edulynx'
+
 import Home from '../components/Home'
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
-    {path: '/', name: 'Edulynx', component: Edulynx},
-    {path: '/home', name: 'Home', component: Home},
+    {path: '/', name: 'Home', component: Home},
+    {path: '/home', name: 'Home', component: Home}
   ]
 });
