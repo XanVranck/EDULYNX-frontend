2810ad7f7564c766bf6ce619be56c149029eb1f5
walegria <william.alegriamira@cegeka.com>
2018-05-18 10:42:08 +0200

    williamm, jeroens: added AddItemDialog

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,33 +1,40 @@
 <template>
-<div>
-    <md-layout v-for="item in items" :key="item">
-        <md-card md-with-hover>
-            <md-ripple>
-                <md-card-header>
-                    <div class="md-title">{{item.title}}</div>
-                </md-card-header>
+    <div>
+        <add-item-dialog/>
+        <md-layout v-for="item in items" :key="item">
+            <md-card md-with-hover>
+                <md-ripple>
+                    <md-card-header>
+                        <div class="md-title">{{item.title}}</div>
+                    </md-card-header>
 
-                <md-card-content>
-                    {{item.summary}}
-                </md-card-content>
+                    <md-card-content>
+                        {{item.summary}}
+                    </md-card-content>
 
-                <md-card-footer>
-                    <star-rating :rating="item.rating" :star-size="20" :read-only="true" :increment="0.5"></star-rating>
-                </md-card-footer>
+                    <md-card-footer>
+                        <star-rating :rating="item.rating" :star-size="20" :read-only="true"
+                                     :increment="0.5"></star-rating>
+                    </md-card-footer>
 
-                <md-card-actions>
-                    <md-button>View</md-button>
-                </md-card-actions>
-            </md-ripple>
-        </md-card>
-    </md-layout>
-</div>
+                    <md-card-actions>
+                        <md-button>View</md-button>
+                    </md-card-actions>
+                </md-ripple>
+            </md-card>
+        </md-layout>
+    </div>
 </template>
 
 <script>
+    import AddItemDialog from './AddItemDialog.vue'
+
     export default {
         name: 'Home',
-        data () {
+        components: {
+            'add-item-dialog': AddItemDialog
+        },
+        data() {
             return {
                 items: [
                     {
@@ -70,7 +77,7 @@
             }
         }
     }
-    
+
 </script>
 
 <style lang="scss" scoped>
