373f411ce47ccdc59aa1ef11b8e39ff192721719
walegria <william.alegriamira@cegeka.com>
2018-05-18 10:33:31 +0200

    williamm, jeroens: Import VueMaterial

--- src/main.js
+++ src/main.js
@@ -3,18 +3,10 @@ import Edulynx from './Edulynx.vue';
 import 'vue-material/dist/vue-material.min.css';
 import router from './router/router.js'
 import 'vue-material/dist/theme/black-green-dark.css'
-import { MdList, MdIcon, MdSwitch, MdCheckbox, MdToolbar, MdContent, MdCard, MdButton, MdLayout } from 'vue-material/dist/components';
+import VueMaterial from 'vue-material'
 import StarRating from 'vue-star-rating';
 
-Vue.use(MdList);
-Vue.use(MdIcon);
-Vue.use(MdSwitch);
-Vue.use(MdCheckbox);
-Vue.use(MdToolbar);
-Vue.use(MdContent);
-Vue.use(MdCard);
-Vue.use(MdButton);
-Vue.use(MdLayout);
+Vue.use(VueMaterial);
 Vue.component('star-rating', StarRating);
 
 Vue.config.productionTip = false;
